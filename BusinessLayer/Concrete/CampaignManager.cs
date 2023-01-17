08d0d8c68d43d0772346ec6132e315a1795935cb
Hamza Kilic <hamza2358@hotmail.com>
2023-01-17 15:15:33 +0300

    finish campaign

---
+++ BusinessLayer/Concrete/CampaignManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Absract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CampaignManager : ICampaignService
+    {
+        ICampaignDal _campaignDal;
+
+        public CampaignManager(ICampaignDal campaignDal)
+        {
+            _campaignDal = campaignDal;
+        }
+
+        public void TAdd(Campaign t)
+        {
+            _campaignDal.Insert(t);
+        }
+
+        public void TDelete(Campaign t)
+        {
+            _campaignDal.DeleteSql(t);
+        }
+
+        public Campaign TGetByID(int id)
+        {
+            return _campaignDal.Get(x => x.CampaignId == id);
+        }
+
+        public List<Campaign> TGetList()
+        {
+            return _campaignDal.GetList();
+        }
+
+        public void TUpdate(Campaign t)
+        {
+            _campaignDal.Update(t);
+        }
+    }
+}
---
+++ DataAccessLayer/EntityFramework/EFCampaignDal.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Absract;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EFCampaignDal : GenericRepository<Campaign>, ICampaignDal
+    {
+    }
+}
