2ab1b226df22acf30647c5fd7d0b05a6d8f553e0
Hamza Kilic <hamza2358@hotmail.com>
2023-02-13 17:01:14 +0300

    add brandcontroller and some fixes

---
+++ E-Ticaret/Controllers/BrandController.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using X.PagedList;
+
+namespace E_Ticaret.Controllers
+{
+	public class BrandController : Controller
+	{
+		BrandManager bm = new BrandManager(new EFBrandDal());
+
+		public IActionResult Index(string f, int p = 1)
+		{
+			if (f == null)
+				f = "";
+			var values = bm.TGetList().Where(x => x.BrandName.ToLower().Contains(f.ToLower())).ToPagedList(p, 10);
+
+			return View(values);
+		}
+        [HttpPost]
+        public IActionResult AddBrand(Brand brand)
+        {
+            brand.BrandStatus = true;
+            bm.TAdd(brand);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteBrand(int id)
+        {
+            var brand = bm.TGetByID(id);
+            if (brand.BrandStatus == true)
+                brand.BrandStatus = false;
+            else
+                brand.BrandStatus = true;
+            bm.TDelete(brand);
+            return RedirectToAction("Index");
+        }
+    }
+}
